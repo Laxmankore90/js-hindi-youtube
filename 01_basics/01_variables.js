abe0fccb992f538c3520701ef92ab772f52755ee
Laxman kore <112858118+Laxmankore90@users.noreply.github.com>
2026-02-12 04:11:57 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const   accountId = 144553
+let accountEmail = "laxmankore@gmail.com"
+var accountPassword = "1213123"
+accountcity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use Strict"; // treat  all js code newer virsion
+
+//alert(3+3) // we are using nodejs , not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+
+     console.log("laxman")
+
+
+
+
+let name ="laxman" // string dataType
+let age = 17  // Number dataType
+let isLoggedIn = false  // boolean dataType
+
+
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
